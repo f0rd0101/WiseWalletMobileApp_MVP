3731dcbe189e9faefaccc505ddbf81282bed2d5f
Nick <melancholic8078@gmail.com>
2025-06-05 02:12:22 +0300

    added vip version

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -5,10 +5,16 @@ from DjangoProject.view import main_view
 from DjangoProject.view import login_page
 from DjangoProject.view import  market_page
 from DjangoProject.view import settings_page
+from DjangoProject.view import main_vip_view
+from DjangoProject.view import market_page_vip
+from DjangoProject.view import settings_vip_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',main_view, name='home'),
     path('login/',login_page, name='login'),
     path('market/',market_page, name='market'),
     path('settings/',settings_page, name='settings'),
+    path('vip_main/',main_vip_view, name='vip'),
+    path('vip_market/',market_page_vip, name='market_vip'),
+    path('vip_settings/',settings_vip_view, name='settings_vip'),
 ]
--- DjangoProject/view.py
+++ DjangoProject/view.py
@@ -10,3 +10,11 @@ def market_page(request):
     return render(request, 'market.html')
 def settings_page(request):
     return render(request, 'settings.html')
+
+def main_vip_view(request):
+    return render(request,'index_vip.html')
+
+def market_page_vip(request):
+    return render(request, 'market_vip.html')
+def settings_vip_view(request):
+    return render(request,'settings_vip.html')
